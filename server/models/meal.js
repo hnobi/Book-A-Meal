c62f4830d8c54eaf229b77d479d312a51fd21ddf
Hammed Noibi <hnobi08@yahoo.com>
2018-04-24 09:20:52 -1100

    create meal model

---
+++ server/models/meal.js
@@ -0,0 +1,19 @@
+const Meals = [
+  {
+    id: 1,
+    title: 'porridge',
+    description: 'made from yam and very good for health',
+    price: 345
+  }, {
+    id: 2,
+    title: 'friend rice',
+    description: 'the yellow rice with a good taste',
+    price: 345
+  }, {
+    id: 3,
+    title: 'pizza',
+    description: 'the yellow rice with a good taste',
+    price: 345
+  }
+];
+export default Meals
