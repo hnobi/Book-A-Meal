31a44d12aaace3b8a41384da35757e7b9bf25437
Hammed Noibi <hnobi08@yahoo.com>
2018-04-25 02:04:09 -1100

    create order model

---
+++ server/controller/order.js
@@ -0,0 +1,31 @@
+const Order = [
+  {
+    id: 1,
+    price: 500,
+    quatity: 3,
+    totalPrice: 1500,
+    userId: 4,
+    meald: 2,
+    menuId: 4
+  },
+  {
+    id: 2,
+    price: 1000,
+    quatity: 3,
+    totalPrice: 3000,
+    userId: 2,
+    meald: 2,
+    menuId: 1
+  },
+  {
+    id: 3,
+    price: 900,
+    quatity: 3,
+    totalPrice: 2700,
+    userId: 1,
+    meald: 1,
+    menuId: 3
+  }
+
+];
+export default Order;
